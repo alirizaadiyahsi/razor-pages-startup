7d89e5dc2f4e14b6d5fadd32d05683e66f6660cb
alirıza adıyahşi <alirizaadiyahsi@gmail.com>
2018-01-31 22:42:30 +0300

    added client libraries

---
+++ src/BaseProjectTemplate.Web.RazorPages/bundle.config.js
@@ -0,0 +1,37 @@
+﻿//Alternative
+
+module.exports = {
+  libsFolder: './wwwroot/lib',
+  mappings: {
+    "node_modules/bootstrap/dist/css": "bootstrap/dist/css"
+  },
+  bundles: [
+      {
+        outputFileName: 'wwwroot/view-resources/Areas/AppAreaName/Views/_Bundles/common-styles.css',
+        inputFiles: [
+            'wwwroot/Common/Styles/**/*.css',
+            'wwwroot/fonts/**/*.css',
+            '!wwwroot/Common/Styles/**/*.min.css'
+        ]
+      },
+      {
+        outputFileName: 'wwwroot/view-resources/Areas/AppAreaName/Views/_Bundles/common-scripts.js',
+        inputFiles: [
+            "wwwroot/Common/Scripts/**/*.js",
+            "!wwwroot/Common/Scripts/**/*min.js"
+        ]
+      },
+      {
+        outputFileName: "wwwroot/js/views/home/index.min.js",
+        inputFiles: [
+            "wwwroot/js/views/home/index.js"
+        ]
+      },
+      {
+        outputFileName: "wwwroot/view-resources/Areas/AppAreaName/Views/AuditLogs/Index.min.css",
+        inputFiles: [
+            "wwwroot/view-resources/Areas/AppAreaName/Views/AuditLogs/Index.css"
+        ]
+      }
+  ]
+};
