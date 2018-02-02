9c5da4a8b477202e911738d974cbcd8b57841efb
alirıza adıyahşi <alirizaadiyahsi@gmail.com>
2018-02-02 23:59:25 +0300

    Modified admin layout

---
+++ src/BaseProjectTemplate.Web.RazorPages/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using BaseProjectTemplate.Core.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BaseProjectTemplate.Web.RazorPages.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, ILogger<AccountController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            return RedirectToPage("/Index");
+        }
+    }
+}
