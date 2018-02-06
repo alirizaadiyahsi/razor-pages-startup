03d34317388e35f4a87c6a237dff5326e3472601
alirıza adıyahşi <alirizaadiyahsi@gmail.com>
2018-02-06 22:55:19 +0300

    Resolved #8 - Add an open source icons library

--- src/BaseProjectTemplate.Web.RazorPages/bundle.config.js
+++ src/BaseProjectTemplate.Web.RazorPages/bundle.config.js
@@ -7,7 +7,8 @@ module.exports = {
     "node_modules/bootstrap/dist/js/bootstrap.js": "bootstrap/dist/js",
     "node_modules/jquery/dist/jquery.js": "jquery/dist",
     "node_modules/jquery-validation/dist/jquery.validate.js": "jquery-validation/dist",
-    "node_modules/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js": "jquery-validation-unobtrusive"
+    "node_modules/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js": "jquery-validation-unobtrusive",
+    "node_modules/font-awesome/css/font-awesome.css":"font-awesome/css"
   },
   bundles: [
 
@@ -41,7 +42,8 @@ module.exports = {
       {
           "outputFileName": "wwwroot/pages/_bundles/layout.min.css",
           "inputFiles": [
-              "wwwroot/lib/bootstrap/dist/css/bootstrap.css"
+              "wwwroot/lib/bootstrap/dist/css/bootstrap.css",
+              "wwwroot/lib/font-awesome/css/font-awesome.css"
           ]
       },
       {
@@ -57,6 +59,21 @@ module.exports = {
               "wwwroot/lib/jquery-validation/dist/jquery.validate.js",
               "wwwroot/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
           ]
-      }
+      },
+      // admin pages resources
+      {
+          "outputFileName": "wwwroot/pages/admin/_bundles/admin-layout.min.css",
+          "inputFiles": [
+              "wwwroot/lib/bootstrap/dist/css/bootstrap.css",
+              "wwwroot/lib/font-awesome/css/font-awesome.css"
+          ]
+      },
+      {
+          "outputFileName": "wwwroot/pages/admin/_bundles/admin-layout.min.js",
+          "inputFiles": [
+              "wwwroot/lib/jquery/dist/jquery.js",
+              "wwwroot/lib/bootstrap/dist/js/bootstrap.js"
+          ]
+      },
   ]
 };
